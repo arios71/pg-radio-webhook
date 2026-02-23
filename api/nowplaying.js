3fb8a359d167a89df4c6e68b763dc3b339b304f9
PGR <arios71@gmail.com>
2026-02-22 21:14:39 -0400

    Update nowplaying.js

--- api/nowplaying.js
+++ api/nowplaying.js
@@ -8,7 +8,7 @@ let currentMetadata = {
 export default async function handler(req, res) {
 
   // ===== CORS =====
-  res.setHeader("Access-Control-Allow-Origin", "https://puragracia.com");
+res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
