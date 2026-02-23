aa807ba63aec169c86ef40c4416d856d726cb227
PGR <arios71@gmail.com>
2026-02-22 23:38:29 -0400

    Update nowplaying.js

--- api/nowplaying.js
+++ api/nowplaying.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   // ===== CORS =====
   // Permite tanto puragracia.com como www.puragracia.com
-  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Origin", "https://puragracia.com, https://www.puragracia.com"); 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
